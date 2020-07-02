89a904a7ec5e49ff8807703c4704069aac703d33
Nibpib <nic.hoffs@gmail.com>
2020-07-02 11:26:19 -0700

    Updates

---
+++ precache-manifest.61186193a243c4a3c672e23d84a7d3cd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19003ec442a0bb343d1f3c5979300926",
+    "url": "/unBoxed/index.html"
+  },
+  {
+    "revision": "2be5ee3040eda3381235",
+    "url": "/unBoxed/static/css/main.caec0e7c.chunk.css"
+  },
+  {
+    "revision": "4dcd470f491517aa912d",
+    "url": "/unBoxed/static/js/2.ee8421f6.chunk.js"
+  },
+  {
+    "revision": "ebec6ec5880fcec562f9a7747ae47b51",
+    "url": "/unBoxed/static/js/2.ee8421f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2be5ee3040eda3381235",
+    "url": "/unBoxed/static/js/main.6f54d7de.chunk.js"
+  },
+  {
+    "revision": "ddbd23fd7acf82cfb3dc",
+    "url": "/unBoxed/static/js/runtime-main.53143c27.js"
+  },
+  {
+    "revision": "e0076ebae251541acb7c83fbb510f71f",
+    "url": "/unBoxed/static/media/berries.e0076eba.jpg"
+  },
+  {
+    "revision": "dd0c72196c78f591214b4b4acb5bd670",
+    "url": "/unBoxed/static/media/logo.dd0c7219.png"
+  }
+]);
